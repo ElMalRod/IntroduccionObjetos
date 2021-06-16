a83729691d5f49ca0b1e7a7267668a2d08b1b855
Emilio Maldonado <luismaldonado201931707@cunoc,edu,gt>
2021-06-16 10:39:18 -0600

    Agrego la Primer clase principal

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+package src;
+
+/**
+ *
+ * @author emili
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+    }
+     public Principal(){
+      
+    }
+    
+}
